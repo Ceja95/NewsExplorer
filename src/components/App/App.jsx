fbd4b5ae036388b43f68117e34a35066d3288ffb
Jose Lara-Ceja <laracejaj@gmail.com>
2026-01-11 16:15:11 -0800

    added States Routes and Navigate

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Navigate, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+return (
+  <Routes>
+    <Route path="/" element={} />
+    <Route path="/saved-news" element={} />
+    <Route path="*" element={} />
+  </Routes>
+);
