1c324696cb981d7fae37b3ad038a4901f711c966
Jose Lara-Ceja <laracejaj@gmail.com>
2026-01-14 14:45:42 -0800

    created preloader file and css

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,10 @@
+import "./Preloader.css";
+
+export default function Preloader() {
+
+    return (
+        <div className="circle-preloader">
+            <p className="circle-preloader__text">Searching for news...</p>
+        </div>
+    )
+}
