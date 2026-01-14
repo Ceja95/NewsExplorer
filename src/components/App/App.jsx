3f1e97b01358c3033930f730c22feb47f13e46f8
Jose Lara-Ceja <laracejaj@gmail.com>
2026-01-14 14:55:38 -0800

    created header file with css and imported into the app file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,25 @@ import { useEffect, useState } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-return (
-  <Routes>
-    <Route path="/" element={<Main />} />
-    <Route path="/saved-news" element={<ProtectedRoute />} />
-    <Route path="*" element={<Navigate to="/" replace />} />
-  </Routes>
-);
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+
+function App() {
+
+  return (
+    <div className="app">
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/saved-news" element={<ProtectedRoute />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      <Header />
+      
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="header-title">NewsExplorer</h1>
+        </header>
+    )
+}
+
+export default Header;
